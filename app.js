c3c7b051705943e6317357242ba84932d2bce1d3
dnoberon <johnw.darrington@gmail.com>
2020-05-07 14:30:19 -0600

    added basic save

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 var express = require('express');
 var app = express();
+var AWS = require("aws-sdk")
+var bodyParser = require("body-parser")
+
+app.use(bodyParser.json())
+
+function getDynamoClient() {
+  AWS.config.update({
+    region: "us-east-1",
+    endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+  })
+
+ return new AWS.DynamoDB.DocumentClient()
+}
+
 
 app.get('/', function(req, res) {
-  res.send({
-    "islandCode": "DOGJ38",
-    "turnipPrice": 589
-  });
+    res.send({
+      "islandCode": "DOGJ38",
+      "turnipPrice": 589,
+    });
 });
 
 app.post('/', function(req, res) {
-  res.send(200);
+    docClient = getDynamoClient()
+
+    var payload = {
+      TableName: "stalk-bot",
+      Item: {
+        "islandCode": req.body.islandCode,
+        "turnipPrice": req.body.turnipPrice,
+        "ttl": Date.now() + (30 * 60 * 1000)
+      }
+    }
+
+  docClient.put(payload, function(err, data) {
+    if (err) {
+      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+
+    res.sendStatus(200);
+  })
 });
 
 
