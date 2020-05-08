db3e2b6ae43e4b595ea34e532d63711e22bad9bf
dnoberon <johnw.darrington@gmail.com>
2020-05-08 13:22:14 -0600

    no more tests

--- tests/test.js
+++ tests/test.js
@@ -7,11 +7,4 @@ const app = require('../app.js');
 const request = supertest(app);
 
 describe('Tests app', function() {
-  it('verifies get', function(done) {
-    request.get('/').expect(200).end(function(err, result) {
-        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
-        done(err);
-    });
-  });
-
 });
